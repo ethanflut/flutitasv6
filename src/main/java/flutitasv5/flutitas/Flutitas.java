24d4ef23208908b13c183dee078956f5b202d9ff
lafrutax <ttjacoboethanx12345@gmail.com>
2022-09-22 22:40:43 -0400

    Initial commit

---
+++ src/main/java/flutitasv5/flutitas/Flutitas.java
@@ -0,0 +1,10 @@
+package flutitasv5.flutitas;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Flutitas implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/flutitasv5/flutitas/client/FlutitasClient.java
@@ -0,0 +1,13 @@
+package flutitasv5.flutitas.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class FlutitasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
